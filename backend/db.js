2bb3b4031f4b87fb36a5378114fdaba785260f5a
Squid6699 <brayanaviles96@gmail.com>
2025-02-19 20:25:08 -0700

    Configure DB

---
+++ backend/db.js
@@ -0,0 +1,8 @@
+import mysql from "mysql2";
+
+export const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.USER_DB,
+    password: process.env.PASS_DB,
+    database: process.env.NAME_DB
+}).promise();
