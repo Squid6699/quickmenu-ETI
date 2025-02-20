506afad57c342a90845458a50fb23091a408c191
Squid6699 <brayanaviles96@gmail.com>
2025-02-19 20:25:30 -0700

    configure initial

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import { middleware } from "./middleware/cors";
+
+
+if (process.env.NODE_ENV === 'production') {
+    dotenv.config({path: "./.env.production"});
+    console.log('Cargando variables de producción');
+} else {
+    dotenv.config({path: "./.env.development"});
+    console.log('Cargando variables de desarrollo');
+}
+
+const app = express();
+app.use(middleware);
+
+
+
+app.listen(process.env.PUERTO, () => {
+    console.log("Conectado backend");
+});
