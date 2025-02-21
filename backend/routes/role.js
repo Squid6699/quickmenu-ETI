b60f3c0ac6170158c4d32d13c184f4cc32e1c0eb
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2025-02-21 14:53:08 -0700

    manage roles

---
+++ backend/routes/role.js
@@ -0,0 +1,62 @@
+import express from "express";
+import { db } from "../db.js";
+
+export const routerAddRoles = express.Router();
+export const routerGetRoles = express.Router();
+export const routerDeleteRoles = express.Router();
+
+
+routerAddRoles.post("/addRoles", async (req, res) => {
+    const { name } = req.body;
+
+    if (!name){
+        return res.status(400).json({msg: "MISSING DATA"});
+    }
+
+    const query = "INSERT INTO role (name) VALUES (?)";
+    db.query(query, [name], (err, result) => {
+        if (err) {
+            return res.status(500).json({msg: "INTERAL SERVER ERROR"});
+        }
+        return res.status(200).json({success: true, msg: "ROLE ADDED"});
+    })
+})
+
+routerGetRoles.get("/getRoles", async (req, res) => {
+    
+    const customHeader = req.headers['x-frontend-header'];
+
+    if (customHeader !== 'frontend') {
+        return res.status(401).send('Unauthorized');
+    }
+
+    const query = "SELECT * FROM role";
+
+    db.query(query, (err, result) => {
+        if (err) {
+            return res.status(500).json({msg: "INTERAL SERVER ERROR"});
+        }
+        return res.status(200).json({success: true, msg: "ROLES RETRIEVED", data: result});
+    })
+})
+
+routerDeleteRoles.delete("/deleteRoles", async (req, res) => {
+    const { id } = req.body;
+
+    if (!id){
+        return res.status(400).json({msg: "MISSING DATA"});
+    }
+
+    const query = "DELETE FROM role WHERE id = ?";
+    db.query(query, [id], (err, result) => { 
+        if (err) {
+            return res.status(500).json({msg: "INTERAL SERVER ERROR"});
+        }
+
+        if (result.affectedRows === 0){
+            return res.status(404).json({success: false, msg: "ROLE NOT FOUND"});
+        }
+
+        return res.status(200).json({success: true, msg: "ROLE DELETED"});
+    })
+})
