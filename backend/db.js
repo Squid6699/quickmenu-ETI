69e015737d10ac9eca34c3a450e5bc23db365161
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2025-02-21 14:54:19 -0700

    modify db

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql2";
 
-export const db = mysql.createPool({
+export const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.USER_DB,
     password: process.env.PASS_DB,
     database: process.env.NAME_DB
-}).promise();
+});
