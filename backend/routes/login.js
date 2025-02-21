ba38bb7e7ba4afa51a41ef74787d168e18b9c59c
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2025-02-21 14:52:34 -0700

    add routes auth

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,17 +1,21 @@
 import express from "express";
 import { db } from "../db.js";
+import  jwt  from "jsonwebtoken";
 
 export const routerLogin = express.Router();
+export const routerRegister = express.Router();
 
-routerLogin.post("/login", (req, res) => {
+routerLogin.post("/login", async (req, res) => {
 
     const {username, password} = req.body;
+    
 
     if (!username || !password){
         return res.status(400).json({msg: "MISSING DATA"});
     }
 
-    const query = "SELECT username, password FROM users WHERE username = ? AND password = ?";
+    const query = "SELECT username, password, name, roleId FROM users WHERE username = ? AND BINARY password = ?";
+
     db.query(query, [username, password], (err, result) => {
         if (err) {
             return res.status(500).json({msg: "INTERAL SERVER ERROR"});
@@ -19,7 +23,26 @@ routerLogin.post("/login", (req, res) => {
         if (result.length === 0){
             return res.status(401).json({success: false, msg: "LOGIN ERROR"});
         }
-        
-        return res.status(200).json({success: true, msg: "LOGIN SUCCESSFUL"});
+        const token = jwt.sign({username: result[0].username, name: result[0].name, roleId: result[0].roleId}, process.env.SECRET_KEY, {expiresIn: process.env.EXPIRES_IN});
+        return res.status(200).json({success: true, msg: "LOGIN SUCCESSFUL", token: token});
+    });
+});
+
+routerRegister.post("/register", async (req, res) => {
+    const {username, password, name, roleId} = req.body;
+
+    if (!username || !password || !name || !roleId){
+        return res.status(400).json({msg: "MISSING DATA"});
+    }
+
+    const query = "INSERT INTO users (username, password, name, roleId) VALUES (? ? ? ?)";
+    db.query(query, [username, password, name, roleId], (err, result) => {
+        if (err) {
+            return res.status(500).json({msg: "INTERAL SERVER ERROR"});
+        }
+        if (result.affectedRows === 0){
+            return res.status(401).json({success: false, msg: "REGISTER ERROR"});
+        }
+        return res.status(200).json({success: true, msg: "REGISTER SUCCESSFUL"});
     })
 })
