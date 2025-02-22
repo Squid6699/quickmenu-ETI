8e4dfd52ac5a58d01f9d5f7d57b1fafa0dc0eb1b
Squid6699 <brayanaviles96@gmail.com>
2025-02-21 23:08:20 -0700

    desing home

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,22 +1,52 @@
-import { Alert, StyleSheet, View } from 'react-native';
+import { Alert, ScrollView, StyleSheet, View, SafeAreaView, ImageBackground, useWindowDimensions } from 'react-native';
 import CustomButton from './components/Button';
 
 export default function App() {
   return (
-    <View style={styles.container}>
 
-      <CustomButton title="Menu" onPress={() => Alert.alert('Menu')} iconName="grid" />
-      <CustomButton title="Ordenar" onPress={() => Alert.alert('Ordenar')} iconName="add" />
-
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <ImageBackground
+        source={require('./assets/background.jpg')}
+        style={styles.background}
+        resizeMode='cover'
+      >
+        <ScrollView contentContainerStyle={styles.body}>
+          <View style={styles.container}>
+            <CustomButton title="Menu" description="Ver opciones" onPress={() => Alert.alert('Menu')} iconName="grid" />
+            <CustomButton title="Ordenar" description="Hacer pedido" onPress={() => Alert.alert('Ordenar')} iconName="add" />
+            <CustomButton title="Mesero" description="Llamar mesero" onPress={() => Alert.alert('Mesero')} iconName="person" />
+            <CustomButton title="Cuenta" description="Pedir cuenta" onPress={() => Alert.alert('Cuenta')} iconName="wallet" />
+          </View>
+        </ScrollView>
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0a0909',
+  },
+  
+  background: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center', // Centra el contenido
+    alignItems: 'center', // Centra el contenido horizontalmente
+  },
+
+  body: {
+    flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 16, // Evita que los botones se peguen a los bordes laterales
+    paddingBottom: 20, // Evita que los botones queden pegados a la gesture bar
+  },
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
