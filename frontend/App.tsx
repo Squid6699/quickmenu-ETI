3fdc97f64f448fdef33f5d3a62a6fbac6ecf3054
Squid6699 <brayanaviles96@gmail.com>
2025-02-28 00:33:37 -0700

    Add page Auth to App

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,53 +1,36 @@
-import { Alert, ScrollView, StyleSheet, View, SafeAreaView, ImageBackground, useWindowDimensions } from 'react-native';
-import ButtonsHome from './components/ButtonsHome';
+import { ImageBackground, SafeAreaView, StyleSheet } from 'react-native';
+import Auth from './pages/Auth';
+import Home from './pages/Home';
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <StatusBar hidden={true}/>
+    <SafeAreaView style={style.safeArea}>
+      <StatusBar hidden={true} />
       <ImageBackground
         source={require('./assets/background.jpg')}
-        style={styles.background}
+        style={style.background}
         resizeMode='cover'
       >
-        <ScrollView contentContainerStyle={styles.body}>
-          <View style={styles.container}>
-            <ButtonsHome title="Menu" description="Ver opciones" onPress={() => Alert.alert('Menu')} iconName="grid" />
-            <ButtonsHome title="Ordenar" description="Hacer pedido" onPress={() => Alert.alert('Ordenar')} iconName="add" />
-            <ButtonsHome title="Mesero" description="Llamar mesero" onPress={() => Alert.alert('Mesero')} iconName="person" />
-            <ButtonsHome title="Cuenta" description="Pedir cuenta" onPress={() => Alert.alert('Cuenta')} iconName="wallet" />
-          </View>
-        </ScrollView>
+        {/* <Home /> */}
+        <Auth />
       </ImageBackground>
+
     </SafeAreaView>
+
   );
 }
 
-const styles = StyleSheet.create({
+export const style = StyleSheet.create({
   safeArea: {
     flex: 1,
     backgroundColor: '#0a0909',
   },
-  
+
   background: {
     ...StyleSheet.absoluteFillObject,
     justifyContent: 'center', // Centra el contenido
     alignItems: 'center', // Centra el contenido horizontalmente
   },
-
-  body: {
-    flexGrow: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 16, // Evita que los botones se peguen a los bordes laterales
-    paddingBottom: 20, // Evita que los botones queden pegados a la gesture bar
-  },
-  container: {
-    width: "97%",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
 });
+
