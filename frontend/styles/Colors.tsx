dd7299df5e031691317b98a97783cb0f22e64332
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2025-03-01 16:26:29 -0700

    create variable colors

---
+++ frontend/styles/Colors.tsx
@@ -0,0 +1,5 @@
+export const backgroundColor = "#efc451";
+export const textColor = "#0a0909";
+export const iconColor = "#fff";
+export const colorError = "#ff0000";
+export const colorSuccess = "#00ff00";
