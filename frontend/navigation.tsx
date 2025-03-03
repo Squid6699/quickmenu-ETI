6a4018fe2229c6c2a16d03492ab4d87d5cb8d42b
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2025-03-02 19:37:09 -0700

    navigation

--- frontend/navigation.tsx
+++ frontend/navigation.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./pages/Home";
+import { useAuth } from "./hook/useAuth";
+import Auth from "./pages/Auth";
 
 const Stack = createStackNavigator();
 
 export default function Navigation() {
+  const { user } = useAuth();
 
   return (
-      <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} options={{headerShown:false}} />
-      </Stack.Navigator>
+    <Stack.Navigator>
+      {
+        user ? (
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        ) : (
+          <Stack.Screen name="Auth" component={Auth} options={{ headerShown: false }} />
+        )
+      }
+    </Stack.Navigator>
   );
 }
