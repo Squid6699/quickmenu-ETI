4b69e5639528bc384b1189bbfacc369e49169efb
Squid6699 <brayanaviles96@gmail.com>
2025-03-04 00:52:17 -0700

    create menuStyles

---
+++ frontend/styles/MenuStyles.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+import { backgroundCard } from './Colors';
+import CardTitle from 'react-native-paper/lib/typescript/components/Card/CardTitle';
+
+export const MenuStyles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        padding: 30
+    },
+
+    activityIndicator: {
+        justifyContent: "center",
+        margin: "auto"
+    },
+
+    Card: {
+        marginVertical: 8, 
+        padding: 10, 
+        backgroundColor: backgroundCard
+    },
+
+    CardTitle: {
+        fontSize: 18, 
+        fontWeight: "bold"
+    },
+
+    CardDescription: {
+        fontSize: 16, 
+        color: "black"
+    },
+
+    CardPrice: {
+        fontSize: 18, 
+        fontWeight: "bold", 
+        marginTop: 5
+    }
+});
