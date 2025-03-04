b69828de623532b109b7714dc32e00fcadd1f443
Squid6699 <brayanaviles96@gmail.com>
2025-03-03 22:11:30 -0700

    add color header

--- frontend/styles/Colors.tsx
+++ frontend/styles/Colors.tsx
@@ -3,3 +3,4 @@ export const textColor = "#0a0909";
 export const iconColor = "#fff";
 export const colorError = "#ff0000";
 export const colorSuccess = "#00ff00";
+export const headerColor = "#171717";
