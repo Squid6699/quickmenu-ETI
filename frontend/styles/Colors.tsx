d5353c2252cf5e5a379d847f9ff6a873c7c55b7e
Squid6699 <brayanaviles96@gmail.com>
2025-03-04 00:51:50 -0700

    add styles card menu

--- frontend/styles/Colors.tsx
+++ frontend/styles/Colors.tsx
@@ -4,3 +4,4 @@ export const iconColor = "#fff";
 export const colorError = "#ff0000";
 export const colorSuccess = "#00ff00";
 export const headerColor = "#171717";
+export const backgroundCard = "rgba(239, 196, 81, 1.0)";
