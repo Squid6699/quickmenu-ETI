9f6b52e5a9f7f0d8c28caef66c79fb7f9e842bed
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2025-03-16 20:46:17 -0700

    Delete colorestsx

--- frontend/styles/Colors.tsx
+++
@@ -1,7 +0,0 @@
-// export const backgroundColor = "#efc451";
-// export const textColor = "#0a0909";
-// export const iconColor = "#fff";
-// export const colorError = "#ff0000";
-// export const colorSuccess = "#00ff00";
-// export const headerColor = "#171717";
-// export const backgroundCard = "rgba(239, 196, 81, 1.0)";
