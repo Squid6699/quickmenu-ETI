c4d8bc9aa9e2f3784991823a1005f48a335a3909
Squid6699 <brayanaviles96@gmail.com>
2025-03-17 20:30:21 -0700

    add icon for password

--- frontend/components/InputText.tsx
+++ frontend/components/InputText.tsx
@@ -1,17 +1,17 @@
+import { useState } from 'react';
 import { TextInput, Text } from 'react-native-paper';
 import { InputTextProps } from '../types';
 import { NativeSyntheticEvent, TextInputChangeEventData } from 'react-native';
 import { useCustomColors } from '../hook/useCustomColors';
 
-const InputText = ({ value, label, placeholder, error, disable, onChange, type="text" }: InputTextProps) => {
-
+const InputText = ({ value, label, placeholder, error, disable, onChange, type = "text" }: InputTextProps) => {
   const { colorError } = useCustomColors();
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
   const handleInputOnchange = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
     const value = event.nativeEvent.text;
     onChange(value);
-  }
-
+  };
 
   return (
     <>
@@ -21,15 +21,22 @@ const InputText = ({ value, label, placeholder, error, disable, onChange, type="
         onChange={handleInputOnchange}
         placeholder={placeholder}
         disabled={disable}
-        secureTextEntry={type === "password" ? true : false}
+        secureTextEntry={type === "password" && !isPasswordVisible}
         keyboardType={type === "number" ? "numeric" : undefined}
-        mode='outlined'
-        multiline={type === "textarea" ? true : false}
+        mode="outlined"
+        multiline={type === "textarea"}
         numberOfLines={type === "textarea" ? 5 : 1}
+        right={
+          type === "password" ? (
+            <TextInput.Icon
+              icon={isPasswordVisible ? "eye-off" : "eye"}
+              onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+            />
+          ) : undefined
+        }
       />
-      {error && <Text style={{color: colorError}} variant="labelLarge">{error}</Text>}
+      {error && <Text style={{ color: colorError }} variant="labelLarge">{error}</Text>}
     </>
-
   );
 };
 
